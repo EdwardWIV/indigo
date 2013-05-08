3e057ee59de06670156c382b078d23d4d6e2bdf4
Giada Barbini <jadefkriv@hotmail.com>
2013-05-08 02:15:46 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Indigo::Application.config.secret_key_base = '88b6bcb387ee08c246e18a1040b19ea14fbd3b9e3e86f5fef8d481aa0b60ca8765fa8015d57477c6fe5a632470e4daa04eabdab5f1c19a9d50c4eaefe1d9924c'
